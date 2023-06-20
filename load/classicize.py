f585028f638b7bc5daaa6d06306659da10084c6c
Walter Michael Shandruk <waltms@anastrophe.lib.uchicago.edu>
2023-06-20 18:42:13 -0500

    Add init for isGreek().

--- load/classicize.py
+++ load/classicize.py
@@ -552,6 +552,14 @@ except:
     init += "from .customRuntime import isEnglish\n"
     show.progress()
 
+#insert isGreek  init
+f = re.search(r'isGreek', init, flags=re.S)
+try:
+    f = f.group()
+except:
+    init += "from .customRuntime import isGreek\n"
+    show.progress()
+
 #insert ObjectFormatter init
 f = re.search(r'format_concordance', init, flags=re.S)
 try:
